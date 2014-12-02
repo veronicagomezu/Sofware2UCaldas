1376a0a5505fc8eb8a3fbad36cb7a77759707011
salas <salas@10.6.74.111>
2014-12-02 11:16:06 -0500

    Primera versión Prueba CVS, se añade nueva clase con una operación basica

---
+++ Software2UCaldas/src/software2ucaldas/Prueba.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package software2ucaldas;
+
+/**
+ *
+ * @author salas
+ */
+public class Prueba
+{
+    private int a, b;
+
+    public Prueba(int a, int b)
+    {
+        this.a = a;
+        this.b = b;
+    }
+    
+    public int sumar(){
+        int res = this.a + this.b;
+        return res;
+    }
+    
+}
